0bc74782682f546b41191cd224df1d0931f91012
k_ozawa <kouta.ozawa@nadia.bz>
2025-03-27 17:43:58 +0900

    環境構築

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # eiken-re-application-diagnostic
+
 英検重願診断ページ
+
+---
+
+```bash
+使用言語： vue
+
+```
---
+++ js/script.js
@@ -0,0 +1,45 @@
+(function () {
+
+ const { createApp, ref, computed, onMounted } = Vue;
+
+ const App = {
+   setup() {
+
+
+     const apiClient = axios.create({
+       baseURL: apiBaseUrl.value,
+       headers: {
+         Accept: "application/json",
+       },
+       responseType: "json",
+       timeout: 10000,
+     });
+
+     const fetchPosts = async () => {
+       try {
+         isLoading.value = true;
+         const res = await apiClient.get(
+           `/posts/${serviceId.value}/groups/pickup?count=3&order=asc&withCategory=1`
+         );
+         posts.value = res.data.posts;
+       } catch (err) {
+         console.error(err);
+         // alert("Failed to fetch posts");
+       } finally {
+         isLoading.value = false;
+       }
+     };
+
+
+     onMounted(() => {
+       fetchPosts();
+     });
+
+     return {
+       // posts,
+       // getFileUrl,
+     };
+   },
+ };
+ createApp(App).mount("#app");
+})();
