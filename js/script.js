87b723b2b65308c5b1816fa963a14603bcff5a01
k_ozawa <kouta.ozawa@nadia.bz>
2025-03-27 18:53:12 +0900

    修正する

--- js/script.js
+++ js/script.js
@@ -5,34 +5,35 @@
  const App = {
    setup() {
 
+    console.log("テスト");
+    
+     // const apiClient = axios.create({
+     //   baseURL: apiBaseUrl.value,
+     //   headers: {
+     //     Accept: "application/json",
+     //   },
+     //   responseType: "json",
+     //   timeout: 10000,
+     // });
 
-     const apiClient = axios.create({
-       baseURL: apiBaseUrl.value,
-       headers: {
-         Accept: "application/json",
-       },
-       responseType: "json",
-       timeout: 10000,
-     });
-
-     const fetchPosts = async () => {
-       try {
-         isLoading.value = true;
-         const res = await apiClient.get(
-           `/posts/${serviceId.value}/groups/pickup?count=3&order=asc&withCategory=1`
-         );
-         posts.value = res.data.posts;
-       } catch (err) {
-         console.error(err);
-         // alert("Failed to fetch posts");
-       } finally {
-         isLoading.value = false;
-       }
-     };
+     // const fetchPosts = async () => {
+     //   try {
+     //     isLoading.value = true;
+     //     const res = await apiClient.get(
+     //       `/posts/${serviceId.value}/groups/pickup?count=3&order=asc&withCategory=1`
+     //     );
+     //     posts.value = res.data.posts;
+     //   } catch (err) {
+     //     console.error(err);
+     //     // alert("Failed to fetch posts");
+     //   } finally {
+     //     isLoading.value = false;
+     //   }
+     // };
 
 
      onMounted(() => {
-       fetchPosts();
+       // fetchPosts();
      });
 
      return {
